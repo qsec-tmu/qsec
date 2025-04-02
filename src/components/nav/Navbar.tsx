5059c413c3ca8a6808dae2cad505599803595e6f
Vorobiy <eugenevorob@gmail.com>
2025-04-01 21:49:39 -0400

    new events changes

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -6,7 +6,9 @@ const Navbar: React.FC = () => {
     <nav className="flex items-center justify-between p-4 bg-white text-black fixed top-0 left-0 w-full z-10 shadow-md">
       {/* Logo Section */}
       <div className="flex items-center space-x-4">
-        <div className="w-16 h-16 bg-purple-500 rounded-full"></div> {/* Purple Logo */}
+        <div className="w-16 h-16 rounded-full">
+          <img src ="src\assets\1738277998717-2208217d-a878-4b79-8d45-8567638453f2_1.jpg"></img>
+          </div> {/* Purple Logo */}
         <span className="text-2xl sm:text-xl md:text-2xl font-bold hidden sm:block">
           Quantum Science and Engineering
         </span>
